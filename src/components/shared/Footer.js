7a08ce85c8a6963d9dc267fdf00c5c5f7944d80d
Luke <lnorman@ad1.systemadmin.com>
2021-11-30 15:47:00 -0800

    footer styled -> tailwind

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,44 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-
-const FooterContainer = styled.div`
-    padding: 0% 6%;
-    display: flex;
-    height: 140px;
-    background: #434653 0% 0% no-repeat padding-box;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    box-sizing: border-box;
-    @media only screen and (max-width : 768px) {
-        flex-direction: column;
-        height: auto;
-    }
-`
-
-const FooterLinkText = styled.div`
-    color: white;
-    font-size: 1.1em;
-    font-weight: 500;
-`
-
-const CopyrightText = styled.div`
-    color: white;
-    font-size: 1.1em;
-    font-weight: 300;
-    text-align: right;
-`
-
-const FooterText = styled.div`
-    font-size: 2.4em;
-    color: white;
-`
 
 function Footer() {
     return (
-        <FooterContainer>
-            <FooterLinkText>
+        <div className="bg-slate w-full h-auto lg:h-32 px-0 lg:px-16 box-border flex flex-col lg:flex-row items-center justify-between">
+            <div className="text-white font-medium text-center lg:text-left">
                 <Link to="/about">
                     <p>About</p>
                 </Link>
@@ -48,16 +14,16 @@ function Footer() {
                 <Link to="/commandments">
                     <p>Stoke List Commandments</p>
                 </Link>
-            </FooterLinkText>
+            </div>
             {/*eslint-disable-next-line*/}
-            <FooterText>
-                //the <b>stoke list.</b>
-            </FooterText>
-            <CopyrightText>
-                <div>Copyright 2009-2020</div>
+            <div className="text-white text-4xl font-light">
+                //the <span className="font-medium">stoke list.</span>
+            </div>
+            <div className="text-white text-center lg:text-right font-light">
+                <div>Copyright 2009-2021</div>
                 <div>All Rights Reserved</div>
-            </CopyrightText>
-        </FooterContainer>
+            </div>
+        </div>
     )
 }
 
