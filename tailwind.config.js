12ef4c497af13628e952897c1eadf1cd52b09539
Luke <lnorman@ad1.systemadmin.com>
2022-02-11 14:57:51 -0800

    breakpoint 960px for responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            lg: '960px',
+        },
         extend: {
             colors: {
                 blue: '#175e88',
