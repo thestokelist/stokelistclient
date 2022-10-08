e0916645c37045098075066e9cba3f3d1a48166b
Luke <lnorman@ad1.systemadmin.com>
2022-10-07 20:23:12 -0700

    don't show container if no advert

--- src/components/adverts/Ad.js
+++ src/components/adverts/Ad.js
@@ -4,13 +4,13 @@ import ReactMarkdown from 'react-markdown'
 import { adContent } from './Ad.md.js'
 
 function Ad() {
-    return (
+    return adContent ?
         <div className="flex rounded gray-border shadow bg-white mx-4 mb-4">
             <div className="no-escape p-4">
                 <ReactMarkdown className="markdown" children={adContent} />
             </div>
-        </div>
-    )
+        </div> : <div></div>
+    
 }
 
 export default Ad
